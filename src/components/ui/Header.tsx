505991273a3388e066fc9a662de5faa42b1e28de
omar2550 <omr222000@gmail.com>
2025-08-21 00:16:53 +0300

    fix: animation

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -9,7 +9,7 @@ const Header = ({ text }: { text: string }) => {
         variants={textVariant()}
         initial="hidden"
         whileInView="show"
-        viewport={{ once: true, amount: 0.25 }}
+        viewport={{ once: true, amount: 0.1 }}
       >
         <h1 className="text-gradient text-3xl sm:text-[2.5rem] md:text-[4rem] text-center font-black uppercase">
           {text}
