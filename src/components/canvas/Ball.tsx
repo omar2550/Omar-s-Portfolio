9eadc39f8173ccc9902f659d8d34589feddbf4e0
omar2550 <omr222000@gmail.com>
2025-08-20 23:44:10 +0300

    fix: 3d balls

--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -17,14 +17,9 @@ const Ball = ({ imgUrl }: { imgUrl: string }) => {
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.3} />
       <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
+      <mesh scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color={"#FFF8eb"}
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
+        <meshStandardMaterial color={"#FFF8eb"} polygonOffsetFactor={-5} />
         <Decal
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
