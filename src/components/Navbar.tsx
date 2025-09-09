7302bd8ac9b23025b2659dde322c5661d14d8382
omar2550 <omr222000@gmail.com>
2025-09-09 21:31:16 +0300

    enhance: navbar UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -131,7 +131,7 @@ const Navbar = ({
               key={`link=${idx}`}
               href={navItem.link}
               className={cn(
-                "relative text-primary items-center flex space-x-1 duration-300 hover:text-primary-light hover:bg-white/10 p-4 rounded-3xl"
+                "relative text-primary items-center flex space-x-1 duration-300 hover:text-primary-light hover:bg-white/10 px-4 py-2 rounded-2xl"
               )}
             >
               <span className="text-sm">{navItem.name}</span>
@@ -139,7 +139,7 @@ const Navbar = ({
           ))}
           <div className="text-sm relative" ref={themeRef}>
             <button
-              className="text-primary items-center flex space-x-1 duration-300 hover:text-primary-light hover:bg-white/10 p-4 rounded-3xl cursor-pointer border-none outline-none"
+              className="text-primary items-center flex space-x-1 duration-300 hover:text-primary-light hover:bg-white/10 px-4 py-2 rounded-2xl cursor-pointer border-none outline-none"
               onClick={() => setThemeMenu(!themeMenu)}
               aria-haspopup="true"
               aria-expanded={themeMenu}
@@ -193,16 +193,16 @@ const Navbar = ({
               <a
                 key={`link=${idx}`}
                 href={navItem.link}
-                className={cn(
-                  "relative text-primary items-center flex space-x-1 duration-300 hover:text-primary-light hover:bg-white/10 p-3 md:p-6 rounded-3xl"
-                )}
+                className={
+                  "relative text-primary items-center flex space-x-1 duration-300 hover:text-primary-light hover:bg-white/10 p-3 rounded-2xl"
+                }
               >
                 <span className="text-sm">{navItem.name}</span>
               </a>
             ))}
             <div className="text-sm relative" ref={themeHamburRef}>
               <button
-                className="text-primary items-center flex space-x-1 duration-300 hover:text-primary-light cursor-pointer hover:bg-white/10 p-3 rounded-3xl border-none outline-none"
+                className="text-primary items-center flex space-x-1 duration-300 hover:text-primary-light cursor-pointer hover:bg-white/10 p-3 rounded-2xl border-none outline-none"
                 onClick={() => setThemeMenu(!themeMenu)}
                 aria-haspopup="true"
                 aria-expanded={themeMenu}
